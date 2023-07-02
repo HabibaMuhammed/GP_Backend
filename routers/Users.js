80f0354b8326c4140690b54a56f96670a3a38c06
aishaelshenawy <aisha.nader@gmail.com>
2023-07-02 15:06:39 +0300

    ft:final inshaallah +1000

--- routers/Users.js
+++ routers/Users.js
@@ -6,11 +6,12 @@ const {
   validatorMiddleware,
   getCurrentUser,
 } = require("../middlewares/UserMW");
+const isAdminMW = require("../middlewares/AdminMW");
 router.post("/register", validatorMiddleware, UserController.register);
 router.patch("/update", getCurrentUser, UserController.updateUser);
 router.patch("/updatePassword", getCurrentUser, UserController.updatePassword);
 router.post("/login", AuthMW, UserController.login);
-router.get("/profile", getCurrentUser, (req, res) => {
+router.get("/profile", getCurrentUser,(req, res) => {
   res.json({ name: req.user.firstName });
 });
 router.delete("/Delete", getCurrentUser, UserController.deleteUser);
